3361e73739280209fa37142e6a080b943b1f328d
Christina <smithcla@yahoo.com>
2018-07-26 16:20:44 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
+ 
+  var config = {
+    apiKey: "AIzaSyB2eNwWfMqenKvTdLnBsDKpEEpA_IRTUCM",
+    authDomain: "reservation-site-d8059.firebaseapp.com",
+    databaseURL: "https://reservation-site-d8059.firebaseio.com",
+    projectId: "reservation-site-d8059",
+    storageBucket: "reservation-site-d8059.appspot.com",
+    messagingSenderId: "652457995835"
+  };
+
+  firebase.initializeApp(config);
+  
+
+  function getReservations() {
+
+    database.ref('reservations').on('value', function (results) {
+
+      var allReservations = results.val();
+
+      var context = {
+        name: allReservations[item].name,
+        day: allReservations[item].day
+
+      };
+
+    });
+  }
+
+  var database = firebase.database();
+
+
+  $('#form').on('submit', function(e) {
+    e.preventDefault();
+  
+
+  var userInput = $('#reservation-name', '#reservation-day').val();
+
+  $('#reservation-name', '#reservation-day').val('')
+
+  var reservationsReference = database.ref('reservations');
+
+  reservationsReference.push({
+    name: userInput,
+    day: userInput
+    
+  });
+
+});
+
+
+
 function initMap() {
 
   navigator.geolocation.getCurrentPosition(function(position) {
@@ -15,8 +66,8 @@ function initMap() {
   });
     
     var marker = new google.maps.Marker({
-      position: userLocation,
-      title: 'User Location',
+      position: {lat: 40.8054491, lng: -73.9654415},
+      title: 'The Drinking Goat',
       map: map
   });
 });
@@ -24,3 +75,6 @@ function initMap() {
   
 
 initMap();
+
+
+
